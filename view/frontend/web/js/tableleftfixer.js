cf7fb750d89deeb74388d88f43701c15b8eb5c97
Marcelo Caetano <marcelo.caetano@funarbe.org.br>
2022-11-09 16:04:40 -0300

    JanelaDeEntrega

--- view/frontend/web/js/tableleftfixer.js
+++ view/frontend/web/js/tableleftfixer.js
@@ -1,1 +1,8 @@
+require(['jquery'], function ($) {
+    'use strict';
 
+    if ($(window).width() < 767) {
+        $(".header-top-right-inner.f-right").appendTo(".page-header.header-style-15");
+    }
+
+});
